67e7d01b2d844a10ccdceb60a103d52f5e01eadc
zhalgasss <zhalgaszh07@gmail.com>
2026-03-16 01:08:33 +0500

    single target and area skills

--- src/com/narxoz/rpg/bridge/AreaSkill.java
+++ src/com/narxoz/rpg/bridge/AreaSkill.java
@@ -3,14 +3,17 @@ package com.narxoz.rpg.bridge;
 import com.narxoz.rpg.composite.CombatNode;
 
 public class AreaSkill extends Skill {
+
     public AreaSkill(String skillName, int basePower, EffectImplementor effect) {
         super(skillName, basePower, effect);
     }
 
     @Override
     public void cast(CombatNode target) {
-        // TODO: Area Bridge action
-        // Apply resolved damage to a composite target.
-        // Tip: Let Composite classes decide how to distribute AOE damage.
+
+        int damage = resolvedDamage();
+
+        target.takeDamage(damage);
+
     }
 }
--- src/com/narxoz/rpg/bridge/SingleTargetSkill.java
+++ src/com/narxoz/rpg/bridge/SingleTargetSkill.java
@@ -3,14 +3,17 @@ package com.narxoz.rpg.bridge;
 import com.narxoz.rpg.composite.CombatNode;
 
 public class SingleTargetSkill extends Skill {
+
     public SingleTargetSkill(String skillName, int basePower, EffectImplementor effect) {
         super(skillName, basePower, effect);
     }
 
     @Override
     public void cast(CombatNode target) {
-        // TODO: Single-target Bridge action
-        // 1) Resolve final damage through effect implementor
-        // 2) Apply to target node
+
+        int damage = resolvedDamage();
+
+        target.takeDamage(damage);
+
     }
 }
